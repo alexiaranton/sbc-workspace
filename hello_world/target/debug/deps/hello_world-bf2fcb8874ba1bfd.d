cd8ad68a7ee5244cf152f050204f651cee5d2eb6
Alexi Myle R. Aranton <59692733+alexiaranton@users.noreply.github.com>
2023-12-09 07:12:33 +0000

    From the bootcamp commit

--- hello_world/target/debug/deps/hello_world-bf2fcb8874ba1bfd.d
+++
@@ -1,5 +0,0 @@
-/workspaces/sbc-workspace/hello_world/target/debug/deps/hello_world-bf2fcb8874ba1bfd: src/lib.rs
-
-/workspaces/sbc-workspace/hello_world/target/debug/deps/hello_world-bf2fcb8874ba1bfd.d: src/lib.rs
-
-src/lib.rs:
